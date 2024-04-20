3dc6789638e5e6562cb015f8d264d2e8dff2ace1
Femi, 'The FATE' Olatubosun <femi@zerocomplex.ai>
2024-04-20 10:51:57 +0100

    Initial commit

---
+++ test/note-taking-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NoteTakingApp from '../lib/note-taking-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/note-taking-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NoteTakingApp.NoteTakingAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
